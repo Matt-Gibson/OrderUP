f656c90307ab44e6ed5ca55368c044bf2ebd0533
Matthew Gibson <gibson.matthew@hotmail.com>
2022-10-29 18:29:27 -0400

    Add switch statement in program.cs

---
+++ OrderUpConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Welcome to OrderUp");
+Console.WriteLine("");
+Console.WriteLine("What would you like to do?");
+
+bool userWishesToContinue = true;
+while (userWishesToContinue)
+{
+    Console.WriteLine("[1] Create New Item");
+    Console.WriteLine("[2] View Current List");
+    Console.WriteLine("[3] Update An Item");
+    Console.WriteLine("[4] Delete An Item");
+    Console.WriteLine("[5] Exit");
+
+    switch (int.Parse(Console.ReadLine()!))
+    {
+        case 1:
+            break;
+        case 2:
+            Console.WriteLine("Current List");
+            break;
+        case 3:
+            Console.WriteLine("Which Item Shall Be Updated?");
+            break;
+        case 4:
+            Console.WriteLine("Which Item Shall Be Deleted?");
+            break;
+        case 5:
+            Console.WriteLine("Goodbye!");
+            userWishesToContinue = false;
+            break;
+        default:
+            break;
+    }
+}
