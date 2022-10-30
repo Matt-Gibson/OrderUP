1a7e495f928c4588ecc1dc7a06d92aadf94ee053
Matthew Gibson <gibson.matthew@hotmail.com>
2022-10-30 16:17:21 -0400

    Create and Read added to menu

--- OrderUpConsole/Program.cs
+++ OrderUpConsole/Program.cs
@@ -1,8 +1,12 @@
-﻿Console.WriteLine("Welcome to OrderUp");
+﻿using LineItemLibrary;
+
+Console.WriteLine("Welcome to OrderUp");
 Console.WriteLine("");
 Console.WriteLine("What would you like to do?");
 
+var itemList = new List<LineItem>();
 bool userWishesToContinue = true;
+
 while (userWishesToContinue)
 {
     Console.WriteLine("[1] Create New Item");
@@ -14,9 +18,26 @@ while (userWishesToContinue)
     switch (int.Parse(Console.ReadLine()!))
     {
         case 1:
+            Console.WriteLine("Enter a Quantity");
+            int quantity = int.Parse(Console.ReadLine()!);
+            Console.WriteLine("Enter a Metal Color");
+            string? metalColor = Console.ReadLine();
+            Console.WriteLine("Enter a Customer Name");
+            string? customerName = Console.ReadLine();
+            Console.WriteLine("Enter a Priority");
+            int priority = int.Parse(Console.ReadLine()!);
+            itemList.Add(new LineItem(quantity, metalColor, customerName, priority));
             break;
         case 2:
             Console.WriteLine("Current List");
+            foreach (var LineItem in itemList)
+            {
+                Console.WriteLine(LineItem.quantity);
+                Console.WriteLine(LineItem.metalColor);
+                Console.WriteLine(LineItem.customerName);
+                Console.WriteLine(LineItem.priorityFactor);
+                Console.WriteLine(LineItem.isMade);
+            }
             break;
         case 3:
             Console.WriteLine("Which Item Shall Be Updated?");
